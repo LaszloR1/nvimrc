9c50698d95d18079deffe3def8a2c36148fb2916
LaszloR1 <r1titanhu@gmail.com>
2024-08-03 11:29:41 +0200

    readding init

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -5,6 +5,9 @@ return {
       'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
       'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
     },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
     lazy = false,
     keys = {
       { '<S-h>', '<Cmd>BufferPrevious<CR>', { desc = 'Prev buffer' } },
